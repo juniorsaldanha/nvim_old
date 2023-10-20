1c6b63fc769e88cc00cb7e7e9b29b95baa0d9ff8
Junior Saldanha <umsaldanha@gmail.com>
2023-10-20 20:50:47 -0300

    added some new settings to git-blame

--- lua/custom/configs/git-blame.lua
+++ lua/custom/configs/git-blame.lua
@@ -2,4 +2,7 @@ local git_blame = require "gitblame"
 
 git_blame.setup {
   enabled = true,
+  message_template = "   <author> • <date> • <summary>",
+  message_when_not_committed = "   Oh noes, not committed yet!",
+  date_format = "%r",
 }
